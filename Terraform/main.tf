80130e892193ba74d09769a62615450ee1e0d24b
danade002 <danielademeso1@gmail.com>
2025-09-06 07:13:09 +0100

    setup for advanced CICD

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -70,7 +70,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_ssh" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.devops-class-vpc.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
